ba81cb11ae8e75e32a832923f15ecaf4f2e1b7af
Kavi Dey <kaviasher@gmail.com>
2022-06-23 09:51:39 -0700

    Add ROS dependencies and start integrating open3d

--- grasp_generation/heuristic_grasp_server.py
+++ grasp_generation/heuristic_grasp_server.py
@@ -2,43 +2,85 @@
 
 from __future__ import print_function
 
-from aurmr_web_interface.srv import GenerateHeuristicGrasp, GenerateHeuristicGraspResponse
+from aurmr_web_interface.srv import (
+    GenerateHeuristicGrasp,
+    GenerateHeuristicGraspResponse,
+)
 from sensor_msgs.msg import CameraInfo, Image
 from rospy.numpy_msg import numpy_msg
+from cv_bridge import CvBridge
 import numpy as np
 import rospy
+import open3d as o3d
+
+import aurmr_web_interface
+
+bridge = CvBridge()
 
 camera_intrinsics = []
 color = []
 depth = []
 
+viz_geo = o3d.geometry.PointCloud()
+viz = o3d.visualization.Visualizer()
+viz.create_window()
+viz.add_geometry(viz_geo)
+
+
 def camera_info_callback(camera_info):
     global camera_intrinsics
-    camera_intrinsics = np.reshape(camera_info.P, (3,4))
+    camera_intrinsics = np.reshape(camera_info.P, (3, 4))
 
 
 def color_image_callback(color_image):
     global color
-    color = np.frombuffer(color_image.data, dtype=np.uint8).reshape(color_image.height, color_image.width, -1)
+    color = np.frombuffer(color_image.data, dtype=np.uint8).reshape(
+        color_image.height, color_image.width, -1
+    )
+
 
 def depth_image_callback(depth_image):
     global depth
-    depth = np.frombuffer(depth_image.data, dtype=np.uint8).reshape(depth_image.height, depth_image.width, -1)
+    depth = np.frombuffer(depth_image.data, dtype=np.uint8).reshape(
+        depth_image.height, depth_image.width, -1
+    )
+
+    pcd = utils.create_o3d_pcd(color, depth, camera_intrinsics)
+
+    viz_geo.points = pcd.points
+    viz_geo.colors = pcd.colors
+    viz.update_geometry(viz_geo)
+    viz.poll_events()
+    viz.update_renderer()
+
 
 def handle_generate_heuristic_grasp(req):
     print(req.se2.x, req.se2.y, req.se2.theta)
     return GenerateHeuristicGraspResponse(result="test")
 
+
 def generate_heuristic_grasp_server():
-    rospy.init_node('generate_heuristic_grasp_server')
+    rospy.init_node("generate_heuristic_grasp_server")
 
-    rospy.Service('generate_heuristic_grasp', GenerateHeuristicGrasp, handle_generate_heuristic_grasp)
+    rospy.Service(
+        "generate_heuristic_grasp",
+        GenerateHeuristicGrasp,
+        handle_generate_heuristic_grasp,
+    )
 
-    rospy.Subscriber("/camera_wrist/color/camera_info", CameraInfo, camera_info_callback)
-    rospy.Subscriber("/camera_wrist/color/image_raw", numpy_msg(Image), color_image_callback)
-    rospy.Subscriber("/camera_wrist/depth/image_raw", numpy_msg(Image), depth_image_callback)
-    
+    rospy.Subscriber(
+        "/camera_wrist/color/camera_info", CameraInfo, camera_info_callback
+    )
+    rospy.Subscriber(
+        "/camera_wrist/color/image_raw", numpy_msg(Image), color_image_callback
+    )
+    rospy.Subscriber(
+        "/camera_wrist/depth/image_raw", numpy_msg(Image), depth_image_callback
+    )
+
     rospy.spin()
+
+    viz.destroy_window()
 
 if __name__ == "__main__":
     generate_heuristic_grasp_server()
--- grasp_generation/utils.py
+++ grasp_generation/utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+import open3d as o3d
+
 
 def mask_to_index(mask):
     idx = []
@@ -7,10 +9,29 @@ def mask_to_index(mask):
             idx.append(i)
     return idx
 
+
 def index_to_mask(idx, mask_len):
     mask = [0] * mask_len
 
     for i in idx:
         mask[i] = 1
-    
+
     return mask
+
+
+def create_o3d_pcd(color_img, depth_img, camera_intrinsics):
+    o3d_color_img = o3d.geometry.Image(color_img.astype(np.uint8))
+    o3d_depth_img = o3d.geometry.Image(depth_img.astype(np.float32))
+
+    o3d_camera_intrinsics = o3d.camera.PinholeCameraIntrinsic()
+    o3d_camera_intrinsics.intrinsic_matrix = camera_intrinsics
+
+    o3d_rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(
+        o3d_color_img, o3d_depth_img, convert_rgb_to_intensity=False
+    )
+
+    o3d_pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
+        o3d_rgbd_image, o3d_camera_intrinsics
+    )
+
+    return o3d_pcd
